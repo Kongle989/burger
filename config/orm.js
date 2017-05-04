1b1884ddfa57e32e1b5800bd5ce4909012514544
Kong Le <Kong Le>
2017-05-04 15:06:59 -0400

    orm and connection

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+require('./connection');
+
+var orm = function () {
+    this.selectAll = function () {
+
+    };
+    this.insertOne = function () {
+
+    };
+    this.updateOne = function () {
+
+    };
+};
+
+module.exports = orm;
